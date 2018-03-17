68f8b038babb28007bbbfe28c0d70661556772da
Romane Green <Romane71193@gmail.com>
2018-03-17 13:06:10 -0400

    added favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+
+ import React from 'react';
+ import ReactDOM from 'react-dom';
  require('./index.scss');
 
 
-alert('hiiiii');
 
+const Intro = () => {
+    return (
+    <div id = 'header'>
+    
+    <h1>HACKERNOON</h1>
+    <p>How hackers start their afternoons</p>
+   
+   
+    <ul>
+       <li>AI</li>
+       <li>BLOCKCHAIN</li>
+       <li>VC</li>
+       <li>SWSC HACKATHON</li>
+           </ul>
+    </div>
+    )
+   }
+
+
+ReactDOM.render(
+    
+ <Intro />,
+ document.getElementById('header')
+);
